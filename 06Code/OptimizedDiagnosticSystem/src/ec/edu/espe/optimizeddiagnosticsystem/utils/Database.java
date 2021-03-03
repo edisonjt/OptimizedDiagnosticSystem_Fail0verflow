a9c9b814fa9032888ecb7ce3f497d7b5b091c2c2
luistavaris <luistavaris@hotmail.es>
2021-03-03 00:17:17 -0500

    create method conection BaseData

--- 06Code/OptimizedDiagnosticSystem/src/ec/edu/espe/optimizeddiagnosticsystem/utils/Database.java
+++ 06Code/OptimizedDiagnosticSystem/src/ec/edu/espe/optimizeddiagnosticsystem/utils/Database.java
@@ -10,10 +10,15 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import ec.edu.espe.optimizeddiagnosticsystem.model.Diagnostic;
 import ec.edu.espe.optimizeddiagnosticsystem.model.Doctor;
 import ec.edu.espe.optimizeddiagnosticsystem.model.Nurse;
 import ec.edu.espe.optimizeddiagnosticsystem.model.Patient;
+import org.bson.Document;
 
 /**
  *
@@ -21,14 +26,27 @@ import ec.edu.espe.optimizeddiagnosticsystem.model.Patient;
  */
 public class Database {
 
-    DB database;
+    DB dataBase;
     DBCollection collection;
     BasicDBObject mainDocument = new BasicDBObject();
 
     public Database() {
-        Mongo mongo = new Mongo("localhost", 27017);
-        database = mongo.getDB("FailOverflow");
-        //System.out.println("conectada");
+        try {
+            MongoClientURI uri = new MongoClientURI(
+                    "mongodb+srv://tentacle:atlas1234@cluster0.pq2gf.mongodb.net/myFirstDB?retryWrites=true&w=majority");
+
+            MongoClient mongoClient = new MongoClient(uri);
+            MongoDatabase database = mongoClient.getDatabase("myfirstDB");
+            MongoCollection<Document> collections = database.getCollection("myfirstDB");
+            dataBase = mongoClient.getDB("myfirstDB");
+            collection = dataBase.getCollection("myfirstDB");
+            
+        } catch (Exception ex) {
+            System.out.println("The connection was unsuccesfull");
+        }
+        
+        
+        
     }
 
     public void id(String id) {
@@ -53,7 +71,7 @@ public class Database {
 
     public void dbUsersPassword(String name, String password) {
 
-        collection = database.getCollection("Users Password");
+        collection = dataBase.getCollection("Users Password");
         BasicDBObject document = new BasicDBObject();
 
         document.put("Name", name);
@@ -106,7 +124,7 @@ public class Database {
     }
 
     public void saveDatabase(String option, BasicDBObject basicObject, String dBCollection) {
-        collection = database.getCollection(dBCollection);
+        collection = dataBase.getCollection(dBCollection);
 
         if ("Register".equals(option)) {
             collection.insert(basicObject);
@@ -116,7 +134,7 @@ public class Database {
     }
 
     public void readClinicHistory(String search, String basicObject, String dBCollection) {
-        collection = database.getCollection(dBCollection);
+        collection = dataBase.getCollection(dBCollection);
 
         BasicDBObject consultation = new BasicDBObject();
         consultation.put(basicObject, search);
@@ -129,7 +147,7 @@ public class Database {
     }
 
     public void readDoctor(String search, String basicObject, String dBCollection) {
-        collection = database.getCollection(dBCollection);
+        collection = dataBase.getCollection(dBCollection);
 
         BasicDBObject consultation = new BasicDBObject();
         consultation.put(basicObject, search);
@@ -142,7 +160,7 @@ public class Database {
     }
 
     public void readNurse(String search, String basicObject, String dBCollection) {
-        collection = database.getCollection(dBCollection);
+        collection = dataBase.getCollection(dBCollection);
 
         BasicDBObject consultation = new BasicDBObject();
         consultation.put(basicObject, search);
@@ -190,7 +208,7 @@ public class Database {
     }
 
     public void deleteObject(String search, String basicObject, String dBCollection, boolean decision) {
-        collection = database.getCollection(dBCollection);
+        collection = dataBase.getCollection(dBCollection);
         if (decision) {
             collection.remove(new BasicDBObject().append(basicObject, search));
         }
